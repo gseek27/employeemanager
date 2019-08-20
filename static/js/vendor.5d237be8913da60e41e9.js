8c1d79ea7171233a7fee3f8c7ae9f15935da4e7f
gseek27 <gosalseek27@gmail.com>
2019-08-19 20:03:18 -0700

    new google button26

--- static/js/vendor.5d237be8913da60e41e9.js
+++ static/js/vendor.5d237be8913da60e41e9.js
@@ -2193,5 +2193,21 @@ var ln,fn=function(){function t(){this.repos_={},this.useRestClient_=!1}return t
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-console.warn("\nIt looks like you're using the development build of the Firebase JS SDK.\nWhen deploying Firebase apps to production, it is advisable to only import\nthe individual SDK components you intend to use.\n\nFor the module builds, these are available in the following manner\n(replace <PACKAGE> with the name of a component - i.e. auth, database, etc):\n\nCommonJS Modules:\nconst firebase = require('firebase/app');\nrequire('firebase/<PACKAGE>');\n\nES Modules:\nimport firebase from 'firebase/app';\nimport 'firebase/<PACKAGE>';\n\nTypescript:\nimport * as firebase from 'firebase/app';\nimport 'firebase/<PACKAGE>';\n"),t.exports=i}});
-//# sourceMappingURL=vendor.52501b7b58d8b1066fe7.js.map
+console.warn("\nIt looks like you're using the development build of the Firebase JS SDK.\nWhen deploying Firebase apps to production, it is advisable to only import\nthe individual SDK components you intend to use.\n\nFor the module builds, these are available in the following manner\n(replace <PACKAGE> with the name of a component - i.e. auth, database, etc):\n\nCommonJS Modules:\nconst firebase = require('firebase/app');\nrequire('firebase/<PACKAGE>');\n\nES Modules:\nimport firebase from 'firebase/app';\nimport 'firebase/<PACKAGE>';\n\nTypescript:\nimport * as firebase from 'firebase/app';\nimport 'firebase/<PACKAGE>';\n"),t.exports=i},yviF:function(t,e,n){"use strict";var r,i=(r=n("O4tu"))&&"object"==typeof r&&"default"in r?r.default:r;
+/**
+ * @license
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */t.exports=i}});
+//# sourceMappingURL=vendor.5d237be8913da60e41e9.js.map
